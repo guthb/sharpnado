695a5ccc9e01acb8d443e5fb347fac478c5658e9
Bradley Guthrie <guppy96@gmail.com>
2020-04-28 19:38:25 -0500

    cleaned up examples and summing grades now

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -6,24 +7,21 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            var x = 34.1;
-            var y = 10.3;
-            //var result = x + y;
+            var numbers = new[] { 12.7, 10.3, 6.11, 4.1 };
+
+            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+            grades.Add(56.1);
+
+
+            var result = 0.0;
+            foreach (var number in grades)
+            {
+
+                result += number;
+
+            }
+
             Console.WriteLine(result);
-            var numbers = new double[3];
-
-            numbers[0] = 12.7;
-            numbers[1] = 10.3;
-            numbers[2] = 6.11;
-
-            var result = numbers[0];
-            result = result + numbers[1];
-            result = result + numbers[2];
-            Console.WriteLine(result);
-
-
-
-
 
             if (args.Length > 0)
             {
