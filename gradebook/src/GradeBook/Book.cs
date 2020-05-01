fff439a381da0203eee114d86e77f258bb264b4e
Bradley Guthrie <guppy96@gmail.com>
2020-04-30 20:48:08 -0500

    added some more grades

--- gradebook/src/GradeBook/Book.cs
+++ gradebook/src/GradeBook/Book.cs
@@ -1,10 +1,20 @@
+using System.Collections.Generic;
+
 namespace GradeBook
 {
     public class Book
     {
+        public Book()
+        {
+            grades = new List<double>();
+        }
+
         public void AddGrade(double grade)
         {
-
+            grades.Add(grade);
         }
+
+        //field
+        List<double> grades;
     }
 }
--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -10,7 +10,6 @@ namespace GradeBook
             var book = new Book();
             book.AddGrade(89.1);
 
-
             var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
             grades.Add(56.1);
 
