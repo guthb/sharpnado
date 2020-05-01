b1bdb82540a72fd59f4b26e6a0852588e5fd6542
Bradley Guthrie <guppy96@gmail.com>
2020-04-30 20:52:47 -0500

    finding the highest grade

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -7,18 +7,32 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            var book = new Book();
+            var book = new Book("big old gradebook");
             book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
 
             var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
             grades.Add(56.1);
 
             var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowestGrade = double.MaxValue;
+
             foreach (var number in grades)
             {
+                // if(number > highGrade){
+                //     highGrade = number
+                // }or...
+                highGrade = Math.Max(number, highGrade);
+
                 result += number;
+
             }
             result = result /= grades.Count;
+
+            Console.WriteLine($"The highest grade is {result:N1}");
+            Console.WriteLine($"The lowest grade is {result:N1}");
             Console.WriteLine($"The average grade is {result:N1}");
 
         }
