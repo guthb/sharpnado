86dd5e68d629979d7ee2fdd44a4add94aa0b1e6f
Bradley Guthrie <guppy96@gmail.com>
2020-05-05 16:22:27 -0500

    third test now passes on value into varible

--- gradebook/test/GradeBook.Tests/TypeTest.cs
+++ gradebook/test/GradeBook.Tests/TypeTest.cs
@@ -11,13 +11,16 @@ namespace GradeBook.Tests
         {
             // arrange
             var book1 = GetBook("Book 1");
-            var book2 = GetBook("Book 2");
+            //var book2 = GetBook("Book 2");
             //act
+            var book2 = book1;
 
 
             //assert
-            Assert.Equal("Book 1", book1.Name);
-            Assert.Equal("Book 2", book2.Name);
+            // Assert.Equal("Book 1", book1.Name);
+            // Assert.Equal("Book 2", book2.Name);
+            Assert.Same(book1, book2);
+            Assert.True(Object.ReferenceEquals(book1, book2));
 
         }
 
