3def2e5fa84b6f92668f64d8808e4ce29e3366f0
Bradley Guthrie <guppy96@gmail.com>
2020-05-05 16:57:11 -0500

    new tests for same and different values

--- gradebook/test/GradeBook.Tests/TypeTest.cs
+++ gradebook/test/GradeBook.Tests/TypeTest.cs
@@ -7,20 +7,42 @@ namespace GradeBook.Tests
 
     {
         [Fact]
-        public void Test1()
+        public void TestName()
+        {
+            //Given
+
+            //When
+
+            //Then
+        }
+
+        [Fact]
+        public void GetBookReturnsSameVaribleValue()
         {
             // arrange
             var book1 = GetBook("Book 1");
-            //var book2 = GetBook("Book 2");
-            //act
             var book2 = book1;
 
 
             //assert
-            // Assert.Equal("Book 1", book1.Name);
-            // Assert.Equal("Book 2", book2.Name);
             Assert.Same(book1, book2);
             Assert.True(Object.ReferenceEquals(book1, book2));
+
+        }
+        [Fact]
+        public void GetBookRetrunDifferentObjects()
+        {
+            var book1 = GetBook("Book 1");
+            var book2 = GetBook("Book 2");
+            //act
+
+
+
+            //assert
+            Assert.Equal("Book 1", book1.Name);
+            Assert.Equal("Book 2", book2.Name);
+            Assert.NotSame(book1, book2)
+
 
         }
 
