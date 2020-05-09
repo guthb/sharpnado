587c6f79580c888beffccf47e52e0298fcff40b1
Bradley Guthrie <guppy96@gmail.com>
2020-05-09 08:53:16 -0500

    added grade value check

--- gradebook/src/GradeBook/Book.cs
+++ gradebook/src/GradeBook/Book.cs
@@ -13,7 +13,11 @@ namespace GradeBook
 
         public void AddGrade(double grade)
         {
-            grades.Add(grade);
+            if (grade <= 100 && grade >= 0)
+            {
+                grades.Add(grade);
+            }
+
         }
 
         public Statistics GetStatistics()
