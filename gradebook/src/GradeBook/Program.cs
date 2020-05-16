2ca972a5e6eb009e544faa16ca68f606af2715f0
Bradley Guthrie <guppy96@gmail.com>
2020-05-15 20:13:34 -0500

    a little clean up

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -8,9 +8,6 @@ namespace GradeBook
         static void Main(string[] args)
         {
             var book = new Book("big old gradebook");
-            // book.AddGrade(89.1);
-            // book.AddGrade(90.5);
-            // book.AddGrade(77.5);
             var done = false;
 
             while (!done)
@@ -26,16 +23,12 @@ namespace GradeBook
                 var grade = double.Parse(input);
                 book.AddGrade(grade);
 
-
-
             }
 
             var stats = book.GetStatistics();
 
-
-
-            Console.WriteLine($"The lowest grade is {stats.Low:N1}");
-            Console.WriteLine($"The highest grade is {stats.High:N1}");
+            Console.WriteLine($"The lowest grade is {stats.Low}");
+            Console.WriteLine($"The highest grade is {stats.High}");
             Console.WriteLine($"The average grade is {stats.Average:N1}");
             Console.WriteLine($"The letter grade is {stats.Letter}");
 
