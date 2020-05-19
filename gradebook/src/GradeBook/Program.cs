93bc7ccef0200e7a9017b7525a2b50cae9b6c5be
Bradley Guthrie <guppy96@gmail.com>
2020-05-19 16:28:37 -0500

    new method pulled out of main method

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -10,15 +10,29 @@ namespace GradeBook
             var book = new Book("big old gradebook");
             book.GradeAdded += OnGradeAdded;
 
-            var done = false;
 
-            while (!done)
+            EnterGrades(book);
+
+            var stats = book.GetStatistics();
+
+            Console.WriteLine(Book.CATEGORY);
+            Console.WriteLine($"For the book named {book.Name}");
+            Console.WriteLine($"The lowest grade is {stats.Low}");
+            Console.WriteLine($"The highest grade is {stats.High}");
+            Console.WriteLine($"The average grade is {stats.Average:N1}");
+            Console.WriteLine($"The letter grade is {stats.Letter}");
+
+        }
+
+        private static bool EnterGrades(Book book)
+        {
+            while (true)
             {
                 Console.WriteLine("Enter a grade or 'q' to quit");
                 var input = Console.ReadLine();
+
                 if (input == "q")
                 {
-                    done = true;
                     continue;
                 }
 
@@ -42,19 +56,7 @@ namespace GradeBook
                     Console.WriteLine("**");
                 }
 
-
-
-
             }
-
-            var stats = book.GetStatistics();
-
-            Console.WriteLine(Book.CATEGORY);
-            Console.WriteLine($"For the book named {book.Name}");
-            Console.WriteLine($"The lowest grade is {stats.Low}");
-            Console.WriteLine($"The highest grade is {stats.High}");
-            Console.WriteLine($"The average grade is {stats.Average:N1}");
-            Console.WriteLine($"The letter grade is {stats.Letter}");
 
         }
 
@@ -62,7 +64,6 @@ namespace GradeBook
         {
             Console.WriteLine("A grade was added");
         }
-
 
     }
 }
