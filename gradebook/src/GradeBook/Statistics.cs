29c572467036586003f6d96d1ebc0af45259f123
Bradley Guthrie <guppy96@gmail.com>
2020-05-21 18:51:46 -0500

    defined varibles needed and Add method

--- gradebook/src/GradeBook/Statistics.cs
+++ gradebook/src/GradeBook/Statistics.cs
@@ -6,5 +6,23 @@ namespace GradeBook
         public double High;
         public double Low;
         public char Letter;
+        public double Sum;
+        public int Count;
+
+        public void Add(double number)
+        {
+            Sum += number;
+            Count += 1;
+        }
+
+        public Statistics()
+        {
+            Count = 0;
+            Average = 0.0;
+            High = double.MinValue;
+            Low = double.MaxValue;
+
+        }
+
     }
 }
