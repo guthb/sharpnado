b428cc56d0800df3f4ca596565e6474bf1325aff
Bradley Guthrie <guppy96@gmail.com>
2020-05-21 19:01:20 -0500

    moved switch into a letter property

--- gradebook/src/GradeBook/Statistics.cs
+++ gradebook/src/GradeBook/Statistics.cs
@@ -13,7 +13,31 @@ namespace GradeBook
         }
         public double High;
         public double Low;
-        public char Letter;
+
+        public char Letter
+        {
+            get
+            {
+                switch (Average)
+                {
+                    case var d when d >= 90.0:
+                        return 'A';
+
+                    case var d when d >= 80.0:
+                        return 'B';
+
+                    case var d when d >= 70.0:
+                        return 'C';
+
+                    case var d when d >= 60.0:
+                        return 'D';
+
+                    default:
+                        return 'F';
+
+                }
+            }
+        }
         public double Sum;
         public int Count;
 
