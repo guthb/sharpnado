ae2679ceb418dd97cdf2b776e2add972693c67d4
Bradley Guthrie <guppy96@gmail.com>
2020-06-15 06:03:08 -0500

    linq

---
+++ Cslinq/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Cslinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumeral<string> cites = new[] { "Ghent", "London", "Las Vegas", "Hyderbad" };
+
+
+            foreach (var city in cities)
+            {
+                Console.WriteLine(city);
+            }
+        }
+    }
+}
