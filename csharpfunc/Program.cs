42e37ce4e23392cdf984941b429b97b07ddca664
Bradley Guthrie <guppy96@gmail.com>
2020-06-28 07:14:18 -0500

    setting up varibles

---
+++ csharpfunc/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace csharpfunc
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(args[0]);
+            var result = IsPrime(number);
+
+            Console.WriteLine(result);
+        }
+
+
+    }
+}
