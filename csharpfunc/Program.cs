1cbcc7275d2e7bd3839670d6f09a17b7387d9219
Bradley Guthrie <guppy96@gmail.com>
2020-07-02 17:33:09 -0500

    more examples

--- csharpfunc/Program.cs
+++ csharpfunc/Program.cs
@@ -57,56 +57,56 @@ namespace csharpfunc
             Console.WriteLine(elapsed);
         
 
-        private static IEnumerable<int> GetRandomNumbers()
-        {
-            // yield return 3;
-            // yield return 6;
-            // yield return 8;
-            // yield return 9;
-            // yield return 11;
-            // yield return 13;
-            Random rand = new Random();
-            while (true)
+            private static IEnumerable<int> GetRandomNumbers()
             {
-                yield return rand.Next(1000);
-            }
-
-        }
-
-        private static bool IsPrime(int number)
-        {
-            bool result = true;
-            for (long i = 2; i < number; i++)
-            {
-                if (number % i == 0)
+                // yield return 3;
+                // yield return 6;
+                // yield return 8;
+                // yield return 9;
+                // yield return 11;
+                // yield return 13;
+                Random rand = new Random();
+                while (true)
                 {
-                    result = false;
-                    break;
+                    yield return rand.Next(1000);
                 }
+
             }
-            return result;
-        }
 
-        private static IEnumerable<int> Find(this IEnumerable<int> values, Func<int, bool> test)
-        {
-            // var result = new List<int>();
-
-            foreach (var number in values)
+            private static bool IsPrime(int number)
             {
-                Console.WriteLine("Testing {0}", number);
-                if (test(number))
+                bool result = true;
+                for (long i = 2; i < number; i++)
                 {
-                    //result.Add(number);
-                    yield return number;
+                    if (number % i == 0)
+                    {
+                        result = false;
+                        break;
+                    }
                 }
+                return result;
             }
-            return result;
 
-        }
+            private static IEnumerable<int> Find(this IEnumerable<int> values, Func<int, bool> test)
+            {
+                // var result = new List<int>();
 
+                foreach (var number in values)
+                {
+                    Console.WriteLine("Testing {0}", number);
+                    if (test(number))
+                    {
+                        //result.Add(number);
+                        yield return number;
+                    }
+                }
+                return result;
+
+            }
+        
         private static bool IsOdd(int number) { }
 
         private static bool IsEven(int number) { }
-
+        
     }
 }
