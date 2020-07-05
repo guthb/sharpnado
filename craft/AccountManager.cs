be98bb389be86ac145806b59f0d44984fb835a56
Bradley Guthrie <guppy96@gmail.com>
2020-07-05 14:26:41 -0500

    rule 10 avoid regions

---
+++ craft/AccountManager.cs
@@ -0,0 +1,15 @@
+namespace craft
+{
+    public class AccountManager
+    {
+        //Avoid regions
+        #region UserManagment
+        #endregion
+
+        #region RoleManagmen
+        #endregion
+
+        #region DataAccess
+        #endregion
+    }
+}
