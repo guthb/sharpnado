f1d08c4e17c7f202ba7cf5e52e9481bea3ac150c
Bradley Guthrie <guppy96@gmail.com>
2020-07-07 21:16:30 -0500

    new effort

---
+++ beginner/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace beginner
+{
+    class Program
+    {
+        sstatic void Main(string[] args)
+    {
+        string venue = "C's Blues";
+        int bands = 4;
+        Console.WriteLine(venue + " will have " + bands + " bands performing tonight!" );
+    }
+    }
+}
