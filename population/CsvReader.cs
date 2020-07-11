e09e7747ae6114e5fc69d84653d93ee6fb40de5e
Bradley Guthrie <guppy96@gmail.com>
2020-07-11 16:36:36 -0500

    reader class star

--- population/CsvReader.cs
+++ population/CsvReader.cs
@@ -2,6 +2,15 @@ namespace population
 {
     public class CsvReader
     {
-        
+        private string _csvFilePath;
+        public CsvReader (string csvFilePath)
+        {
+            this._csvFilePath = csvFilePath;
+        }
+
+        public Country[] ReadFirstNCountries(int nCountries)
+        {
+            return null;
+        }
     }
 }
