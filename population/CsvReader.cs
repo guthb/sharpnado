a09587814f99860aa64c8040da99b73bd4c3ac23
Bradley Guthrie <guppy96@gmail.com>
2020-07-12 10:09:03 -0500

    split was set up incorrectly for data in file

--- population/CsvReader.cs
+++ population/CsvReader.cs
@@ -17,8 +17,8 @@ namespace population
 
         public Country ReadCountryFromCsvLine(string csvLine)
         {
-            string[] parts = csvLine.Split(',');
-            string name=parts[0];
+            string[] parts = csvLine.Split(new char[] {','});
+            string name = parts[0];
             string code =parts[1];
             string region = parts[2];
             int population = int.Parse(parts[3]);
