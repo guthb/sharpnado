2fc88ba2194e24cf4449bfa6da8c9094bdac92cb
Bradley Guthrie <guppy96@gmail.com>
2020-07-13 16:16:20 -0500

    first method for format

--- population/PopulationFormatter.cs
+++ population/PopulationFormatter.cs
@@ -2,6 +2,12 @@ namespace population
 {
     public class PopulationFormatter
     {
-        //to do
+        public static string FormatPopulation(int population)
+        {
+            if (population == 0)
+                return "(Unknown)"
+            int popRounded = RoundPopulation4(population);
+            return $"{popRounded:### ### ### ###}".Trim();
+        }
     }
 }
