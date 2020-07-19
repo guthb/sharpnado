53a5583c7fb78bc316d307be0ee46076939cdbbe
Bradley Guthrie <guppy96@gmail.com>
2020-07-19 16:11:17 -0500

    enumerat items in dictionary

--- population/Program.cs
+++ population/Program.cs
@@ -55,19 +55,19 @@ namespace population
             country norway = new Country("Norway", "NOR", "Europe", 5_282_223);
             country finland = new Country("Finland", "FIN", "Europe", 5_511_303);
 
-            //Dictionary<string, Country> countries = new Dictionary<string, Country>();
-
             //better:
             var countries = new Dictionary<string, Country>();
-            // countries.Add("NOR, norway");
-            // countries.Add("FIN, finland");
-
+         
             //better
             countries.Add(norway.Code, norway);
             countries.Add(finland.Code, finland);
 
             Country selectedCountry = countries["NOR"];
             Console.WriteLine(selectedCountry.Name);
+
+            foreach (var country in countries.Values)        
+                Console.WriteLine(coutnry.Value.Name);
+            
         
         }
     }
