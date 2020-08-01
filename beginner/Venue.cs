30c31486d8183f242a2a59a0b8c85ba8cad739d5
Bradley Guthrie <guppy96@gmail.com>
2020-07-31 19:31:50 -0500

    new class

---
+++ beginner/Venue.cs
@@ -0,0 +1,13 @@
+namespace beginner
+{
+    public class Venue
+    {
+        public string name;
+        public void Announce()
+        {
+            Console.WriteLine(name + " will have bands performing tonight.");    
+        }
+
+    }
+
+}
