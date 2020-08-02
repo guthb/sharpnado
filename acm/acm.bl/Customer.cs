ea800a61d0b7eeef880ae57156d9417298df4800
Bradley Guthrie <guppy96@gmail.com>
2020-08-02 07:48:27 -0500

    encapsulation

--- acm/acm.bl/Customer.cs
+++ acm/acm.bl/Customer.cs
@@ -2,7 +2,14 @@ namespace acm.bl
 {
     public class Customer
     {
+        public int CustomerId {get; private set;}
+        
+        public string EmailAddress {get; set;}
+        
+        public string FirstName {get; set;}
+
         private string _lastName;
+        
         public string _lastName
         {
             get
