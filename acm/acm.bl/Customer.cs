d8ec4c7c40a54a9891a3bfaf4d5045d36ab88f85
Bradley Guthrie <guppy96@gmail.com>
2020-08-02 07:50:43 -0500

    ready for unit test

--- acm/acm.bl/Customer.cs
+++ acm/acm.bl/Customer.cs
@@ -2,12 +2,21 @@ namespace acm.bl
 {
     public class Customer
     {
+        
         public int CustomerId {get; private set;}
         
         public string EmailAddress {get; set;}
         
         public string FirstName {get; set;}
 
+        public string FullName
+        {
+            get
+                {
+                    return LastName + "," + FirstName;
+                }
+        }
+        
         private string _lastName;
         
         public string _lastName
