a8094d1e112914b4d0985a1698fa220deaacb514
Bradley Guthrie <guppy96@gmail.com>
2020-08-03 21:03:07 -0500

    unit test for customer name

---
+++ acm/unittest/acm.bl.test/CustomerTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using acm.bl;
+
+namespace acm.bl.test
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {
+            //-- Arrange
+            Customer customer = new Customer
+            {
+                FirstName = "Bilbo",
+                LastName = "Baggins"
+            };
+            string expected "Baggins, Bilbo";
+         
+            // -- Act
+            string actual = customer.FullName;
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
