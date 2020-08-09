444762fc77a73fb56f0d1ca33bf3f3ab42fb5002
Bradley Guthrie <guppy96@gmail.com>
2020-08-09 07:23:34 -0500

    repo units test class created

---
+++ acm/unittest/acm.bl.test/CustomerRepositoryTest.cs
@@ -0,0 +1,18 @@
+
+namespace acm.bl.test
+{
+    [TestClass]
+    public class CustomerRepositoryTest
+    {
+        [TestMethod]
+        public void RetrieveValid()
+        {
+            // --Arrange
+
+            // --Act
+
+            // --Assert
+
+        }
+    }
+}
