839e846428a2d28bd1bc93df14225b328518d419
Bradley Guthrie <guppy96@gmail.com>
2020-08-11 10:07:13 -0500

    new class

---
+++ acm/acm.bl/OrderRepository.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace acm.bl
+{
+    public class OrderRepository
+    {
+        /// <summary>
+        /// Retrive one order.
+        /// </summary>
+        public Order Retrieve(int orderId)
+        {
+            Order order = new Order(orderId);
+            
+            // retrieves the order
+
+            // hard-coded to return an order
+            if ( orderId == 10)
+            {
+                // Use current year
+                order.OrderDate = new DateTimeOffset(DateTime.Now.Year, 8, 11, 10, 00, 00, new TimeSpan(7, 0, 0));
+            }
+        }
+        
+
+    }
+}
