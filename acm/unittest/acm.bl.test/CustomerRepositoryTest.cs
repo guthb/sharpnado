36468b2897d3f90ba3d3719ced1c03bced069dc4
Bradley Guthrie <guppy96@gmail.com>
2020-08-12 15:29:48 -0500

    start of unit test

--- acm/unittest/acm.bl.test/CustomerRepositoryTest.cs
+++ acm/unittest/acm.bl.test/CustomerRepositoryTest.cs
@@ -1,3 +1,4 @@
+using System;
 using acm.bl;
 
 namespace acm.bl.test
---
+++ acm/unittest/acm.bl.test/OrderRepositoryTest.cs
@@ -0,0 +1,28 @@
+using System;
+using acm.bl;
+
+
+
+namespace acm.bl.test
+{
+    [TestClass()]
+    public class OrderRepositoryTest
+    {
+        [TestClass()]
+        public void RetrieveOrderDisplayTest()
+        {
+            //-- Arrange
+            var orderRepository = new OrderRepositoryTest();
+            var expected = new Order(10)
+            {
+                OderDate = new DateTiemOffset(DateTime.Now.Year, 8, 12, 10 , 00, 00, new TimeSpan(7,0,0))
+            };
+
+            // --Act
+
+
+            // --Assert
+            
+        }
+    }
+}
