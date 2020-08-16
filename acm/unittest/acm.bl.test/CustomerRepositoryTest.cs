a194f46790ddc017d47424193e4bf3396017ba76
Bradley Guthrie <guppy96@gmail.com>
2020-08-16 18:55:28 -0500

    composoition relationship

--- acm/unittest/acm.bl.test/CustomerRepositoryTest.cs
+++ acm/unittest/acm.bl.test/CustomerRepositoryTest.cs
@@ -1,5 +1,6 @@
 using System;
 using acm.bl;
+using System.Collections.Generic;
 
 namespace acm.bl.test
 {
@@ -60,7 +61,31 @@ namespace acm.bl.test
                         PostalCode = "146";
                     }
                 }
-            }
+            };
+
+            //-- Act
+            var actual = customerRepository.Retrive(1);
+
+            //-- Assert
+            Assert.AreEqual(expected.CustomerId, actual.CustmerId);
+            Assert.AreEqual(expected.EmailAddress, actual.EmailAddress);
+            Assert.AreEqual(expected.FirstName, actual.FirstName);
+            Assert.AreEqual(expected.LastName, actual.LastName);
+            Assert.AreEqual(expected.CustomerId, actual.CustmerId);
+
+
+            for (int i =; i < 1; i++)
+            {
+                Assert.AreEqual(expected.AddressList[i].AddressType, actual.AddressList[i].Address);;
+                Assert.AreEqual(expected.AddressList[i].StreetLine1, actual.AddressList[i].StreetLine1);
+                Assert.AreEqual(expected.AddressList[i].StreetLine2, actual.AddressList[i].StreetLine2);
+                Assert.AreEqual(expected.AddressList[i].City, actual.AddressList[i].City);
+                Assert.AreEqual(expected.AddressList[i].Country, actual.AddressList[i].Country);
+                Assert.AreEqual(expected.AddressList[i].PostalCode, actual.AddressList[i].PostalCode)
+            };
+
+
+
 
         }
     }
