ca00ed5743c8d27bc32e533f23ab5f1c5e28a58a
Bradley Guthrie <guppy96@gmail.com>
2020-08-19 08:54:16 -0500

    tests for base class methods

--- acm/unittest/acm.bl.test/ProductRepositoryTest.cs
+++ acm/unittest/acm.bl.test/ProductRepositoryTest.cs
@@ -12,8 +12,8 @@ namespace acm.bl.test
             var ProductRepository = new ProductRepository();
             var expected = new Product(2)
             {
-                Currentprice = 2.00$
-                ProductDescription = "Assorted Flowers"
+                Currentprice = 2.00$,
+                ProductDescription = "Assorted Flowers",
                 ProductName = "Sunflowers"
             };
             
@@ -23,9 +23,48 @@ namespace acm.bl.test
             // --Assert
             Assert.AreEqual(expected.Currentprice, actual.Currentprice);
             Assert.AreEqual(expected.ProductDescription, actual.ProductDescription);
-            Assert.AreEqual(expected.ProductName, actual.ProductName);
+            Assert.AreEqual(expected.ProductName, actual.ProductName);    
+        }
 
-        
+        [TestMethod()]
+        public void SaveTestValid()
+        {
+            // --Arrange
+            var productRepository = new ProductRepository();
+            var updateProduct = new Product(2);
+            {
+                Currentprice = 2.00$,
+                ProductDescription = "Assorted Flowers",
+                ProductName = "Sunflowers",
+                HasChanges = true
+            };
+
+            // --Act
+            var actual = productRepository.Save(updatedProduct);
+
+            // --Assert
+            Assert.AreEqual(true, actual);
+        }
+
+
+        [TestMethod()]
+        public void SaveTestMissingPrice()
+        {
+            // --Arrange
+            var productRepository = new ProductRepository();
+            var updateProduct = new Product(2);
+            {
+                Currentprice = null,
+                ProductDescription = "Assorted Flowers",
+                ProductName = "Sunflowers",
+                HasChanges = true
+            };
+
+            // --Act
+            var actual = productRepository.Save(updatedProduct);
+
+            // --Assert
+            Assert.AreEqual(false, actual);
         }
     }
 }
