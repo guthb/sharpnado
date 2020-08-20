d846ab1337f77bdc850eff820c78817ee08b36cd
Bradley Guthrie <guppy96@gmail.com>
2020-08-20 07:21:56 -0500

    attempting test for common

---
+++ acm/unittest/Acme.CommonTest/StringHandlerTest.cs
@@ -0,0 +1,26 @@
+using System;
+using Acme.Common;
+//using Microsoft.VisualStudio.TestTools.Unitesting;
+
+namespace Acme.CommonTest
+{
+    [TestClass]
+    public class StringHandlerTest
+    {   
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+            var source = "SonicScrewdriver";
+            var expected = "Sonic Screwdriver";
+            var handler = new StringHandler();
+
+            // Act
+            var actual = handler.InserSpaces(source);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
