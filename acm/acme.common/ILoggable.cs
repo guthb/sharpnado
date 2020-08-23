7d139fd3e6708545e4e8e6047a491be2bc0a7ec2
Bradley Guthrie <guppy96@gmail.com>
2020-08-23 18:39:41 -0500

    new interface

---
+++ acm/acme.common/ILoggable.cs
@@ -0,0 +1,13 @@
+using System;
+using Sytem.Collections.Generic;
+using System.Linq;
+using Sytem.Text;
+using System.Threading.Tasks;
+
+namespace acme.common
+{
+    public interface ILoggable
+    {
+        string Log();
+    }
+}
