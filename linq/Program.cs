0161ea131f2555d44ca2333f938045027f6ef1ec
Bradley Guthrie <guppy96@gmail.com>
2020-08-25 19:31:22 -0500

    messing with linq

---
+++ linq/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public void GetUserRecords()
+        {
+            var users = new string[] { "Emily", "Jacob", "Thomas" };
+
+            var userQuery =
+            from user in users
+            select user;
+        }
+    }
+}
