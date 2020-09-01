cca45b9134c1497dbae811cc588b7f70f9020c77
Bradley Guthrie <guppy96@gmail.com>
2020-09-01 07:48:18 -0500

    my version of a linq method

---
+++ LinqSamples/Queries/Movies.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Queries
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public float Rating { get; set; }
+        public int Year { get; set; }
+
+    }
+}
