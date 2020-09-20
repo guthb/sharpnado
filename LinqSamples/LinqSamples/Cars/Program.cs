85b4490d6ef0834ce264ed16febed9a97dfc5fc5
Bradley Guthrie <guppy96@gmail.com>
2020-09-19 21:13:58 -0500

    car stats complete

--- LinqSamples/LinqSamples/Cars/Program.cs
+++ LinqSamples/LinqSamples/Cars/Program.cs
@@ -354,11 +354,20 @@ namespace Cars
 
     public class CarStatistics
     {
-       
-        
-        public CarStatistics Accumulate(Car c)
+
+        public CarStatistics()
         {
+            Max = Int32.MinValue;
+            Min = Int32.MaxValue;
+        }
+
+        public CarStatistics Accumulate(Car car)
+        {
+            Count += 1;
             Total += car.Combined;
+            Max = Math.Max(Max, car.Combined);
+            Min = Math.Min(Min, car.Combined)
+
             return this;
 
         }
@@ -366,6 +375,7 @@ namespace Cars
         public int Max { get; set; }
         public int Min { get; set; }
         public int Total { get; set; }
+        public int Count { get; set; }
         public double Average { get; set; }
     }
 
